cc875120a9829ee757798ce445c260bec7a798b1
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2026-09-24 21:52:54 +0530

    Remove the connection URL for security reasons.

--- S1 - EP13/Code/db.js
+++ S1 - EP13/Code/db.js
@@ -1,8 +1,7 @@
 const { MongoClient } = require("mongodb");
 const { ObjectId } = require("mongodb");
 
-//connection URL
-const url = "mongodb+srv://akshadsantoshjaiswal:rFw7NSxncTpSc7Bq@cluster0.yulixmn.mongodb.net/"
+
 const client = new MongoClient(url);
 
 const dbname = "NamsteNodejs"
